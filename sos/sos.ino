49aafb7ee54737af92fce818fb3f65c2b5b28445
Manav Ahire <rockstarsigma@gmail.com>
2025-09-25 10:45:39 +0530

    Add files via upload

---
+++ sos/sos.ino
@@ -0,0 +1,52 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+// digitalWrite(8,HIGH);
+// delay(150);
+// digitalWrite(8,LOW);
+// delay(150);
+// digitalWrite(8,HIGH);
+// delay(150);
+// digitalWrite(8,LOW);
+// delay(150);
+// digitalWrite(8,HIGH);
+// delay(150);
+// digitalWrite(8,LOW);
+// delay(150);
+
+// delay(300);
+
+// digitalWrite(8,HIGH);
+// delay(300);
+// digitalWrite(8,LOW);
+// delay(100);
+// digitalWrite(8,HIGH);
+// delay(300);
+// digitalWrite(8,LOW);
+// delay(100);
+// digitalWrite(8,HIGH);
+// delay(300);
+// digitalWrite(8,LOW);
+// delay(100);
+
+// delay(300);
+
+// digitalWrite(8,HIGH);
+// delay(150);
+// digitalWrite(8,LOW);
+// delay(150);
+// digitalWrite(8,HIGH);
+// delay(150);
+// digitalWrite(8,LOW);
+// delay(150);
+// digitalWrite(8,HIGH);
+// delay(150);
+// digitalWrite(8,LOW);
+// delay(150);
+
+// delay(1000);
+}
